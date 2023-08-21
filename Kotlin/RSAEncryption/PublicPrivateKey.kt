2265435ea4a773c9783f1dc5efccb990c6ddb907
Fredo Code <128687596+Fredo-Ronan@users.noreply.github.com>
2023-08-21 22:46:22 +0700

    Add files via upload

---
+++ Kotlin/RSAEncryption/PublicPrivateKey.kt
@@ -0,0 +1,44 @@
+import java.math.BigInteger
+
+class PublicPrivateKey {
+
+    companion object {
+        fun generatePublicKey(x_of_n: BigInteger): BigInteger {
+            var i: BigInteger = BigInteger("3")
+
+            while(i.compareTo(x_of_n) == -1){
+
+//                println("Hasil Euclid $i dan $x_of_n: " + EuclidGCD.findEuclidGCD(i, x_of_n))
+//                println("Hasil Euclid $i dan $x_of_n: " + i.gcd(x_of_n))
+                if(i.gcd(x_of_n).compareTo(BigInteger("1")) == 0){
+                    return i
+                }
+
+                i = i.add(BigInteger("1"))
+
+            }
+
+            return BigInteger("-1")
+        }
+
+        fun generatePrivateKey(x_of_n: BigInteger, publicKey: BigInteger): BigInteger{
+            var pk = 3
+
+            while(pk<=x_of_n.toInt()){
+                if (
+                        (
+                            publicKey.multiply(BigInteger(pk.toString()))
+                        )
+                        .mod(x_of_n)
+                        .compareTo(BigInteger("1")) == 0
+                    ){
+                    return BigInteger(pk.toString())
+                }
+                pk++
+            }
+
+            return BigInteger("-1")
+        }
+    }
+
+}
